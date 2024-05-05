c06485de4a9847f06aa27ed7765dc7af7bbdb244
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-05 18:08:43 +0200

    Add the function to change the grid
    The old grid will be removed replaced by a x*x grid with x = user prompt

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const width = 1500;
 let rows = 16;
 let columns = 16;
 
-const firstDivHeight = height / rows;
+let firstDivHeight = height / rows;
 
 for (let i = 0; i < 16; i++) {
     let firstdiv = document.createElement("div");
@@ -22,9 +22,35 @@ for (let i = 0; i < 16; i++) {
 
 const divgrid = document.querySelectorAll(".divgrid");
 
-divgrid.forEach((div) => div.addEventListener("mouseover", () => div.style.backgroundColor = "red"))
+divgrid.forEach((div) => div.addEventListener("mouseover", () => div.style.backgroundColor = "black"))
 
-let button = document.querySelector("button");
-button.addEventListener("click", function() {
-    rows = columns = +prompt("What size do you want ?");
+const button = document.querySelector("button");
+let newSize = 101;
+button.addEventListener("click", () => {
+    newSize = +prompt("What size do you want ?");
+    while (newSize > 100) {
+    newSize = +prompt("What size do you want ?");
+    }
+
+    firstDivHeight = height / newSize;
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (let i = 0; i < newSize; i++) {
+        let firstdiv = document.createElement("div");
+        firstdiv.setAttribute("id", "firstdiv");
+        firstdiv.style.height = `${firstDivHeight}px`;
+        for (let j = 0; j < newSize; j++) {
+            let div = document.createElement("div");
+            div.setAttribute("class", "divgrid")
+            div.style.height = `${firstDivHeight}px`;
+            div.style.width = `${width / newSize}px`;
+            firstdiv.appendChild(div);
+        }
+        container.appendChild(firstdiv);
+    }
+    const divgrid = document.querySelectorAll(".divgrid");
+
+    divgrid.forEach((div) => div.addEventListener("mouseover", () => div.style.backgroundColor = "black"))
 })
