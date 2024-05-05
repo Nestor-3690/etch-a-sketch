9cbdf563fef421b78982a67ae9329db7932b2a64
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-05 18:29:53 +0200

    Add createGrid function to avoid repetition
    Function is used to create the first grid and grids with prompted values

--- script.js
+++ script.js
@@ -1,56 +1,40 @@
 const container = document.querySelector("#grid-container");
-const height = 960;
-const width = 1500;
-let rows = 16;
-let columns = 16;
+const size = 960;
+let squares = 16;
 
-let firstDivHeight = height / rows;
-
-for (let i = 0; i < 16; i++) {
-    let firstdiv = document.createElement("div");
-    firstdiv.setAttribute("id", "firstdiv");
-    firstdiv.style.height = `${firstDivHeight}px`;
-    for (let j = 0; j < 16; j++) {
-        let div = document.createElement("div");
-        div.setAttribute("class", "divgrid")
-        div.style.height = `${firstDivHeight}px`;
-        div.style.width = `${width / columns}px`;
-        firstdiv.appendChild(div);
-    }
-    container.appendChild(firstdiv);
-}
-
-const divgrid = document.querySelectorAll(".divgrid");
-
-divgrid.forEach((div) => div.addEventListener("mouseover", () => div.style.backgroundColor = "black"))
+createGrid(size, squares);
 
 const button = document.querySelector("button");
-let newSize = 101;
+squares = 101;
 button.addEventListener("click", () => {
-    newSize = +prompt("What size do you want ?");
-    while (newSize > 100) {
-    newSize = +prompt("What size do you want ?");
+    squares = +prompt("What size do you want ?");
+    while (squares > 100) {
+    squares = +prompt("What size do you want ?");
     }
-
-    firstDivHeight = height / newSize;
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
+    createGrid(size, squares)
+})
 
-    for (let i = 0; i < newSize; i++) {
+function createGrid(size, squares) {
+    let firstDivHeight = size / squares;
+
+    for (let i = 0; i < squares; i++) {
         let firstdiv = document.createElement("div");
         firstdiv.setAttribute("id", "firstdiv");
         firstdiv.style.height = `${firstDivHeight}px`;
-        for (let j = 0; j < newSize; j++) {
+        for (let j = 0; j < squares; j++) {
             let div = document.createElement("div");
             div.setAttribute("class", "divgrid")
             div.style.height = `${firstDivHeight}px`;
-            div.style.width = `${width / newSize}px`;
+            div.style.width = `${size / squares}px`;
             firstdiv.appendChild(div);
         }
         container.appendChild(firstdiv);
     }
+
     const divgrid = document.querySelectorAll(".divgrid");
 
     divgrid.forEach((div) => div.addEventListener("mouseover", () => div.style.backgroundColor = "black"))
-})
+}
