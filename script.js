fb9077eaedf0888af896ff88d9643caa7ca04b8b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-05 17:32:32 +0200

    Add button to index pages and styles to css file
    Button implemented in js to allow user to change the size of the grid

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const container = document.querySelector("#grid-container");
+const height = 960;
+const width = 1500;
+let rows = 16;
+let columns = 16;
+
+const firstDivHeight = height / rows;
 
 for (let i = 0; i < 16; i++) {
     let firstdiv = document.createElement("div");
     firstdiv.setAttribute("id", "firstdiv");
+    firstdiv.style.height = `${firstDivHeight}px`;
     for (let j = 0; j < 16; j++) {
         let div = document.createElement("div");
         div.setAttribute("class", "divgrid")
+        div.style.height = `${firstDivHeight}px`;
+        div.style.width = `${width / columns}px`;
         firstdiv.appendChild(div);
     }
     container.appendChild(firstdiv);
@@ -14,3 +23,8 @@ for (let i = 0; i < 16; i++) {
 const divgrid = document.querySelectorAll(".divgrid");
 
 divgrid.forEach((div) => div.addEventListener("mouseover", () => div.style.backgroundColor = "red"))
+
+let button = document.querySelector("button");
+button.addEventListener("click", function() {
+    rows = columns = +prompt("What size do you want ?");
+})
