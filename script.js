150518e2655adcd42a1dfe7131aa7ad82e4843b1
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-06 10:09:47 +0200

    Add comments to script.js

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const container = document.querySelector("#grid-container");
 const size = 960;
 let squares = 16;
 
+// CREATE first grid (squares for 16*16) with size of 960px
 createGrid(size, squares);
 
 const button = document.querySelector("button");
-squares = 101;
+// WHEN button clicked
 button.addEventListener("click", () => {
+    // ASK the user for number of squares
     squares = +prompt("What size do you want ?");
+    // WHILE squares > 100, keep asking the user
     while (squares > 100) {
     squares = +prompt("What size do you want ?");
     }
+    // REMOVE previous grid
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
@@ -18,16 +22,18 @@ button.addEventListener("click", () => {
 })
 
 function createGrid(size, squares) {
-    let firstDivHeight = size / squares;
+    let divHeight = size / squares;
 
     for (let i = 0; i < squares; i++) {
         let firstdiv = document.createElement("div");
         firstdiv.setAttribute("id", "firstdiv");
-        firstdiv.style.height = `${firstDivHeight}px`;
+        // SET the div height
+        firstdiv.style.height = `${divHeight}px`;
         for (let j = 0; j < squares; j++) {
             let div = document.createElement("div");
             div.setAttribute("class", "divgrid")
-            div.style.height = `${firstDivHeight}px`;
+            // SET the div height and width
+            div.style.height = `${divHeight}px`;
             div.style.width = `${size / squares}px`;
             firstdiv.appendChild(div);
         }
@@ -36,5 +42,6 @@ function createGrid(size, squares) {
 
     const divgrid = document.querySelectorAll(".divgrid");
 
+    // FOR each div, when mouseover, backgroundColor turn black
     divgrid.forEach((div) => div.addEventListener("mouseover", () => div.style.backgroundColor = "black"))
 }
