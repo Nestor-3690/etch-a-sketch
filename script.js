aac439ec51261aeec9fdd06422b1dece48be06d0
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-06 12:07:32 +0200

    Add function and event listener for modifications
    RGB now gives the case a random color instead of black
    Opacity now progressively changes the opacity of the case

--- script.js
+++ script.js
@@ -2,10 +2,35 @@ const container = document.querySelector("#grid-container");
 const size = 700;
 let squares = 16;
 
+const normal = document.querySelector("#normal");
+const rgb = document.querySelector("#rgb");
+const opacity = document.querySelector("#opacity");
+let isNormal = true;
+let isRGB = false;
+let isOpacity = false;
+
 // CREATE first grid (squares for 16*16) with size of 960px
 createGrid(size, squares);
 
-const button = document.querySelector("button");
+normal.addEventListener("click", () => {
+    isNormal = true;
+    isRGB = isOpacity = false;
+    createGrid(size, squares);
+})
+
+rgb.addEventListener("click", () => {
+    isRGB = true;
+    isNormal = isOpacity = false;
+    createGrid(size, squares);
+})
+
+opacity.addEventListener("click", () => {
+    isOpacity = true;
+    isNormal = isRGB = false;
+    createGrid(size, squares);
+})
+
+const button = document.querySelector("#gridsize");
 // WHEN button clicked
 button.addEventListener("click", () => {
     // ASK the user for number of squares
@@ -43,5 +68,28 @@ function createGrid(size, squares) {
     const divgrid = document.querySelectorAll(".divgrid");
 
     // FOR each div, when mouseover, backgroundColor turn black
-    divgrid.forEach((div) => div.addEventListener("mouseover", () => div.style.backgroundColor = "black"))
+    divgrid.forEach((div) => div.addEventListener("mouseover", () => {
+        if (isNormal === true) {
+            div.style.backgroundColor = "black"
+        } else if (isRGB === true) {
+            let colorOne = Math.round((Math.random()) * 255);
+            let colorTwo = Math.round((Math.random()) * 255);
+            let colorThree = Math.round((Math.random()) * 255);
+            div.style.backgroundColor = `rgb(${colorOne}, ${colorTwo}, ${colorThree}`;
+        } else {
+            let actualOpacity = Number(div.style.opacity) * 100;
+            if (actualOpacity !== 0){
+                actualOpacity += 10;
+                if (actualOpacity > 100) {
+                    actualOpacity = 100;
+                }
+                div.style.opacity = `${actualOpacity}%`;
+            } else {
+                actualOpacity = "10%";
+                div.style.opacity = actualOpacity;
+            }
+        }
+    }))
 }
+
+// div.setAttribute("style", `background-color: rgb(${colorOne}, ${colorTwo}, ${colorThree});`)
